c04e18a268810f8ce075423c612470a52c79f8bb
Samuel Nkopuruk <samueleffiong80@gmail.com>
2024-01-22 14:58:21 +0100

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,3 +1,29 @@
 # write the function is_anagram
 def is_anagram(test, original):
-    pass
+    test = test.lower()
+    original = original.lower()
+
+    
+    # assuming test is an anagram of original
+    flag = True
+
+    if len(test) == len(original):
+        new_string = list(test)
+        for i in new_string:
+
+            if i in original:
+                flag = True
+            else:
+                flag = False
+                break
+        
+        if flag:
+            return True
+        else:
+            return False
+        
+    else:
+        return False
+
+
+    
